37fa2dc876f463c3f792ba4b96ded4631a4d2292
Ulhas Vardhan Golchha <golchha21@gmail.com>
2013-07-24 00:18:48 +0530

    Removed Unused GA Directives

--- index.php
+++ index.php
@@ -176,12 +176,6 @@
 				ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
 				var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 			})();
-		  
-			(function() {
-				var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-				ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-				var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-			})();
 
 			(function(d, s, id) {
 				var js, fjs = d.getElementsByTagName(s)[0];
